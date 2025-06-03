7c1544e5b9f22a2c617859180863497e5a00683d
Javier <35864946+jmagit@users.noreply.github.com>
2025-06-03 16:35:18 +0200

    Pruebas

--- demos/src/main/java/com/examples/ws/soap/Calculadora.java
+++ demos/src/main/java/com/examples/ws/soap/Calculadora.java
@@ -8,8 +8,8 @@ public class Calculadora {
 		return a + b;
 	}
 	public double suma(double a, double b) {
-		return (a + b);
-//		return roundIEEE754(a + b);
+//		return (a + b);
+		return roundIEEE754(a + b);
 	}
 	
 	public int divide(int a, int b) {
@@ -17,10 +17,11 @@ public class Calculadora {
 	}
 	
 	public double divide(double a, double b) {
-//		if(b == 0) {
-//			throw new ArithmeticException("/ by zero");
-//		}
+		if(b == 0) {
+			throw new ArithmeticException("/ by zero");
+		}
 		return roundIEEE754(a / b);
+//		return (a / b);
 	}
 	
 	private double roundIEEE754(double o) {
