2319cc5b5ca292a1aee057453572c3aee8cb3dc9
Javier <35864946+jmagit@users.noreply.github.com>
2025-06-04 16:37:18 +0200

    servlets

---
+++ demos/src/main/java/com/examples/presentation/web/Saluda.java
@@ -0,0 +1,50 @@
+package com.examples.presentation.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Saluda
+ */
+@WebServlet({ "/Saluda", "/saluda", "/saluda/*" })
+public class Saluda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Saluda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/xml");
+		response.setStatus(202, getServletInfo());
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html><html>");
+		out.println("<head><title>Curso</title></head>");
+		out.println("<body>");
+		out.println("<h1>Hola mundo</h1>");
+		out.println("<div>" + request.getMethod() +"</div>");
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
